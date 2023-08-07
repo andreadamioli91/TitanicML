a489a0a54248e0b93bd171048216063972414c6a
Damioli Andrea <Andrea.Damioli@eoc.ch>
2023-08-07 14:27:07 +0200

    Add also scalers

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from loader.csvloader import CsvLoader
 from loader.datavisualizer import DataVisualizer
 from sklearn.pipeline import Pipeline
@@ -7,30 +9,34 @@ from transformer.transformComplex import TransformComplex
 from transformer.dummies import Dummies
 from transformer.typeconverter import TypeConverter
 from transformer.lowcorrelateddrop import DropLowCorrelated
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
 
 TRAIN_DATASET = "data/train.csv"
 TEST_DATASET = "data/test.csv"
+MIN_CORRELATION = 0.05
 
 loader = CsvLoader()
 data_visualizer = DataVisualizer()
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    pipeline = Pipeline([
+    transformer_pipeline = Pipeline([
         ("fillNa", FillNa()),
         ("uselessDrop", UselessDrop()),
         ("transformComplex", TransformComplex()),
         ("dummies", Dummies()),
         ("typeConvert", TypeConverter()),
-        ("dropLowCorrelated", DropLowCorrelated())
+        ("dropLowCorrelated", DropLowCorrelated(MIN_CORRELATION)),
     ])
+    X_initial = loader.load_data(TRAIN_DATASET)
+    X_transformed = transformer_pipeline.fit_transform(X_initial)
 
-    df = loader.load_data(TRAIN_DATASET)
+    scaler_pipeline = Pipeline([
+        ('standardScaler', StandardScaler()),
+        ('toDataFrame', FunctionTransformer(lambda x: pd.DataFrame(x, columns=X_transformed.columns)))
+    ])
+    X_scaled = scaler_pipeline.fit_transform(X_transformed)
 
-    X_transformed = pipeline.fit_transform(df)
-
-    # data_visualizer.show_correlation_heatmap(df, "Survived")
+    data_visualizer.show_correlation_heatmap(X_transformed, "Survived")
     data_visualizer.print_dataframe(X_transformed, 5, None)
-    print("End of the script")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("End of the script")
--- transformer/lowcorrelateddrop.py
+++ transformer/lowcorrelateddrop.py
@@ -1,10 +1,12 @@
-
 class DropLowCorrelated:
+
+    def __init__(self, threshold):
+        self.threshold = threshold
 
     def fit(self, df, y=None):
         numeric_columns = df.select_dtypes(include=['int64', 'float64']).columns
         correlations = df[numeric_columns].corr()['Survived'].drop('Survived')
-        self.columns_to_drop = correlations[abs(correlations) < 0.05].index
+        self.columns_to_drop = correlations[abs(correlations) < self.threshold].index
         return self
 
     def transform(self, df):
